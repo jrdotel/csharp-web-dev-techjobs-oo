da67f1307876f117271eeca3b73ae86b5a625833
jrdotel <Goodjuju22!>
2020-11-01 22:47:13 -0600

    Commit Tests = Passed

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TechJobsOO
+ 
 {
     public class Job
     {
@@ -36,6 +39,60 @@ namespace TechJobsOO
         public override int GetHashCode()
         {
             return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            string jobString = "";
+            var name = Name;
+            var employer = EmployerName.Value;
+            var location = EmployerLocation.Value;
+            var type = JobType.Value;
+            var competency = JobCoreCompetency.Value;
+
+
+            if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(employer) && string.IsNullOrEmpty(location) && string.IsNullOrEmpty(type) && string.IsNullOrEmpty(competency))
+            {
+                jobString = "\n\nOOPS! This job does not seem to exist.\n\n";
+            }
+            else if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(employer) || string.IsNullOrEmpty(location) || string.IsNullOrEmpty(type) || string.IsNullOrEmpty(competency))
+            {
+                string nullMessage = "Data not available";
+
+                if (string.IsNullOrEmpty(name))
+                {
+                    name = nullMessage;
+                }
+
+                if (string.IsNullOrEmpty(employer))
+                {
+                    employer = nullMessage;
+                }
+
+                if (string.IsNullOrEmpty(location))
+                {
+                    location = nullMessage;
+                }
+
+                if (string.IsNullOrEmpty(type))
+                {
+                    type = nullMessage;
+                }
+
+                if (string.IsNullOrEmpty(competency))
+                {
+                    competency = nullMessage;
+                }
+
+                jobString = $"\n\nID: {this.Id}\nName: {name}\nEmployer: {employer}\nLocation: {location}\nPosition Type: {type}\nCore Competency: {competency}\n\n";
+            }
+            else
+            {
+                jobString = $"\n\nID: {this.Id}\nName: {name}\nEmployer: {employer}\nLocation: {location}\nPosition Type: {type}\nCore Competency: {competency}\n\n";
+            }
+
+            
+            return jobString;
         }
     }
 }
--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
 
 namespace TechJobsTests
 {
@@ -8,8 +9,8 @@ namespace TechJobsTests
         [TestMethod]
         public void TestSettingJobId_IDsDifferByOne_ReturnsTrue()
         {
-            var job1 = new TechJobsOO.Job();
-            var job2 = new TechJobsOO.Job();
+            var job1 = new Job();
+            var job2 = new Job();
 
             Assert.AreEqual(job1.Id, job2.Id, 1);
         }
@@ -18,12 +19,12 @@ namespace TechJobsTests
         public void TestJonConstrictorSetsAllFields_AllFieldsSet_ReturnsTrue()
         {
             var name = "Product tester";
-            var employer = new TechJobsOO.Employer("Acme");
-            var location = new TechJobsOO.Location("Desert");
-            var quality = new TechJobsOO.PositionType("Quality control");
-            var coreCompetency = new TechJobsOO.CoreCompetency("Persistence");
+            var employer = new Employer("Acme");
+            var location = new Location("Desert");
+            var quality = new PositionType("Quality control");
+            var coreCompetency = new CoreCompetency("Persistence");
 
-            var job1 = new TechJobsOO.Job(name, employer, location, quality, coreCompetency);
+            var job1 = new Job(name, employer, location, quality, coreCompetency);
 
             Assert.AreEqual(name, job1.Name);
             Assert.AreEqual(employer, job1.EmployerName);
@@ -33,18 +34,75 @@ namespace TechJobsTests
         }
 
         [TestMethod]
-        public void TestJobsForEquality_CheckForId_ReturnsFalse()
+        public void TestJobsForEquality_CheckForIdEquality_ReturnsFalse()
+        {
+            var job1 = new Job();
+            var job2 = new Job();
+
+            Assert.AreNotEqual(job1, job2);
+        }
+
+        [TestMethod]
+        public void TestJobsEmptyLines_CheckForEmptyLines_ReturnTrue()
         {
             var name = "Product tester";
-            var employer = new TechJobsOO.Employer("Acme");
-            var location = new TechJobsOO.Location("Desert");
-            var quality = new TechJobsOO.PositionType("Quality control");
-            var coreCompetency = new TechJobsOO.CoreCompetency("Persistence");
+            var employer = new Employer("Acme");
+            var location = new Location("Desert");
+            var quality = new PositionType("Quality control");
+            var coreCompetency = new CoreCompetency("Persistence");
 
-            var job1 = new TechJobsOO.Job(name, employer, location, quality, coreCompetency);
-            var job2 = new TechJobsOO.Job(name, employer, location, quality, coreCompetency);
+            var job1 = new Job(name, employer, location, quality, coreCompetency);
+            var testChar = '\n';
 
-            Assert.AreNotEqual(job1.Id, job2.Id);
+            Assert.AreEqual(testChar, job1.ToString()[0]);
+            Assert.AreEqual(testChar, job1.ToString()[1]);
+            Assert.AreEqual(testChar, job1.ToString()[job1.ToString().Length - 1]);
+            Assert.AreEqual(testChar, job1.ToString()[job1.ToString().Length - 2]);
+        }
+
+        [TestMethod]
+        public void TestJobsToStringMethod_CheckForToStringMethod_ReturnsTrue()
+        {
+            var name = "Product tester";
+            var employer = new Employer("Acme");
+            var location = new Location("Desert");
+            var quality = new PositionType("Quality control");
+            var coreCompetency = new CoreCompetency("Persistence");
+
+            var job1 = new Job(name, employer, location, quality, coreCompetency);
+            var testString = $"\n\nID: {job1.Id}\nName: Product tester\nEmployer: Acme\nLocation: Desert\nPosition Type: Quality control\nCore Competency: Persistence\n\n";
+
+            Assert.AreEqual(testString, job1.ToString());
+        }
+
+        [TestMethod]
+        public void TestJobsNullFields_CheckForNullFields_ReturnsTrue()
+        {
+            var name = "Product tester";
+            var employer = new Employer("");
+            var location = new Location("Desert");
+            var type = new PositionType("");
+            var coreCompetency = new CoreCompetency("Persistence");
+
+            var job1 = new Job(name, employer, location, type, coreCompetency);
+            var testString = $"\n\nID: {job1.Id}\nName: Product tester\nEmployer: Data not available\nLocation: Desert\nPosition Type: Data not available\nCore Competency: Persistence\n\n";
+
+            Assert.AreEqual(testString, job1.ToString());
+        }
+
+        [TestMethod]
+        public void TestJobsEmptyLines_CheckForAllEmptyFields_ReturnTrue()
+        {
+            var name = "";
+            var employer = new Employer("");
+            var location = new Location("");
+            var type = new PositionType("");
+            var coreCompetency = new CoreCompetency("");
+
+            var job1 = new Job(name, employer, location, type, coreCompetency);
+            string testString = "\n\nOOPS! This job does not seem to exist.\n\n";
+
+            Assert.AreEqual(testString, job1.ToString());
         }
     }
 }
